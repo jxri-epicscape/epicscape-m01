0bea6a79516caadb89c0ac397f9abce8876f8cfa
jxri-epicscape <jyri@epicscape.game>
2025-09-05 13:49:05 +0300

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // tärkeä Netlify-julkaisuissa
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
